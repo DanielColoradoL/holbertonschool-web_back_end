f118e865cce690c7885df2e2c10dc8bf5494d3c5
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-01 14:55:39 +1000

    feat(task 12):log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,20 +8,20 @@ All data was given, and it's avaiable on the intranet
 
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    total = collection.count_documents({})
+    print(f"{total} logs")
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
-total = collection.count_documents({})
-print(f"{total} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("Methods:")
-for method in methods:
-    count = collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
+    filt = {"method": "GET", "path": "/status"}
+    count = collection.count_documents(filt)
+    print(f"{count} status check")
 
-filt = {"method": "GET", "path": "/status"}
-count = collection.count_documents(filt)
-print(f"{count} status check")
-
-client.close()
+    client.close()
