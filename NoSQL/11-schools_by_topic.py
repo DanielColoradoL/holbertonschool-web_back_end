436a14d792948798e2e22d29b6fb4c5d78b64447
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-01 14:05:09 +1000

    feat(task 11):where can i learn python

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Task 11"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    output = []
+    query = {"topics": "C"}
+    for doc in mongo_collection.find(query):
+        print(doc)
+        output.append(doc)
+    return output
