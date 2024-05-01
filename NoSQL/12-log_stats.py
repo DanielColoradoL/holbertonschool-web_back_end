bb7fe25ca7fd03a067bc68e6df42eeb8c0575480
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-01 15:10:57 +1000

    fix(task 12):made the file executable

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,20 +7,20 @@ All data was given, and it's avaiable on the intranet
 """
 from pymongo import MongoClient
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    total = collection.count_documents({})
+    print("{} logs".format(total))
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-collection = client.logs.nginx
-total = collection.count_documents({})
-print("{} logs".format(total))
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("Methods:")
-for method in methods:
-    count = collection.count_documents({"method": method})
-    print("\tmethod {}: {}".format(method, count))
+    filt = {"method": "GET", "path": "/status"}
+    count = collection.count_documents(filt)
+    print("{} status check".format(count))
 
-filt = {"method": "GET", "path": "/status"}
-count = collection.count_documents(filt)
-print("{} status check".format(count))
-
-client.close()
+    client.close()
