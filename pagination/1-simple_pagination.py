89e45969fd248ed189640af61378e49e2513440d
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-05 16:12:44 +1000

    feat(task 1):Simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Return a tuple of size two
+    containing a start index and an end index
+    """
+    start = page * page_size - page_size
+    finish = page * page_size
+    return (start, finish)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return small dataset out of dataset
+        based on page and page size
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        list_index = index_range(page, page_size)
+        return self.__dataset[list_index[0]: list_index[1]]
