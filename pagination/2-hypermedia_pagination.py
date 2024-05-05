51b86dd6b1470b2f734c5b89c3ef8d0018369d06
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-05 17:10:10 +1000

    feat(task 2):Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ task 1 """
 import csv
-from typing import List
+from typing import List, Dict
 
 
 def index_range(page, page_size):
@@ -41,3 +41,27 @@ class Server:
         assert page > 0 and page_size > 0
         list_index = index_range(page, page_size)
         return self.__dataset[list_index[0]: list_index[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """
+        returns a dictionary containing the following key-value pairs:
+        page_size, page, data, next_page, prev_page, total_pages
+        """
+        self.dataset()
+
+        output = {}
+        output["page_size"] = page_size
+        output["page"] = page
+
+        no_pages = round(len(self.__dataset) / page_size)
+        next_page = page + 1 if page <= no_pages else None
+        prev_page = page - 1 if page > 0 else None
+
+        output["total_pages"] = no_pages
+        output["next_page"] = next_page
+        output["prev_page"] = prev_page
+
+        list_index = index_range(page, page_size)
+        output["data"] = self.__dataset[list_index[0]: list_index[1]]
+
+        return output
