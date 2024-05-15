3b3a3d49e57c2a4a59c2e263e820ea26c95fa837
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-15 16:41:55 +1000

    fix(task 2):typo changed Success to success

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ body: 'Success', status: 200 }))
+    .then(() => ({ body: 'success', status: 200 }))
     .catch(() => new Error())
     .finally(console.log('Got a response from the API'));
 }
