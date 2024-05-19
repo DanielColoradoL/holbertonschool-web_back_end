c69ba25eb6973a02e7cd12fa3ed8a1035fe49da7
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-19 21:17:06 +1000

    fix(task 5):added promise.reject

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
 export default function uploadPhoto(fileName) {
-  return new Promise((reject) => reject(new Error(`${fileName} cannot be processed`)));
+  return new Promise.reject(new Error(`${fileName} cannot be processed`));
 }
