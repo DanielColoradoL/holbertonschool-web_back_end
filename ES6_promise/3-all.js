b7dad8a9cd2c388e9cfd8fd45e9a8ae3425f7d34
Daniel Colorado <danielcoloradol@hotmail.com>
2024-05-19 20:40:49 +1000

    fix(task 3):returned the promise object

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((out) => {
-      console.log(`${out[0].body} ${out[1].firstName} ${out[1].lastName}`);
-    })
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((val) => console.log(`${val[0].body} ${val[1].firstName} ${val[1].lastName}`))
     .catch(() => console.log('Signup system offline'));
 }
